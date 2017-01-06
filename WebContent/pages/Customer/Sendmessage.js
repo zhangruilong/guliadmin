d879ec360f50ce5005fcea6ce81d4ab08e7b5979
zhangruilong <zhangruilong@192.168.31.153>
2017-01-06 10:36:31 +0800

    附件

--- WebContent/pages/Customer/Sendmessage.js
+++ WebContent/pages/Customer/Sendmessage.js
@@ -1,34 +1,40 @@
 function commonSend(selections){
 	Ext.Msg.prompt('请确认', '<b>提示:</b>请确认要发送当前选择的条目？', function(btn, text) {
 		if (btn == 'ok') {
-			var url = 'http://www.jitmarketing.cn:10001/Geteway.ashx?request={"Action":"SendMessage","Parameters":{"Sign":"谷粒网","SMSContent":"'+text+'","MobileNO":"';
+			var url = 'http://zapi.253.com/msg/HttpBatchSendSM';
+			var mobile = '13656179712';
 			for (var i = 0; i < selections.length; i++) {
-				$.ajax({
-					url : url+selections[i].data["customerphone"]+'"}}',
-					type:"GET",
-					data:{
-					},
-					success:function(resp){
-					},
-					error:function(resp){
-					}
-				});
-//				Ext.Ajax.request({
-////					url : url+selections[i].data["customerphone"]+'"}}',
-//					url : url+'13656179712'+'"}}',
-//					method : 'GET',
-//					params : {
+				if(selections[i].data["customerphone"].length==11)
+					mobile += ',' + selections[i].data["customerphone"]
+			}
+			$.ajax({
+				url : url,
+				type : "POST",
+				data:{
+					account: 'S2b5PQwac',
+					pswd: 'Shanghaiyulian88',
+					needstatus: true,
+					msg: text,
+					mobile: mobile
+				},
+				success:function(resp){
+				},
+				error:function(resp){
+				}
+			});
+//			var url = 'http://www.jitmarketing.cn:10001/Geteway.ashx?request={"Action":"SendMessage","Parameters":{"Sign":"谷粒网","SMSContent":"'+text+'","MobileNO":"';
+//			for (var i = 0; i < selections.length; i++) {
+//				$.ajax({
+//					url : url+selections[i].data["customerphone"]+'"}}',
+//					type:"GET",
+//					data:{
 //					},
-//					success : function(response) {
-//						var resp = Ext.decode(response.responseText); 
-//						Ext.Msg.alert('提示', resp.msg, function(){
-//						});
+//					success:function(resp){
 //					},
-//					failure : function(response) {
-//						Ext.Msg.alert('提示', '网络出现问题，请稍后再试');
+//					error:function(resp){
 //					}
 //				});
-			};
+//			};
 		}
 	});
 }
